e470b8dfeefa876d5fdf2595400d23485e50305e
YuliiaKostishyna <112808612+YuliiaKostishyna@users.noreply.github.com>
2023-05-01 21:56:50 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import Statistics from './Statistics/Statistics ';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions ';
 import Section from './Section/Section';
